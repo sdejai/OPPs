48717e9e2a0a698f3509acc3aad09cfb016c3251
sdejai <sde.jai.java@gmail.com>
2025-11-17 15:59:09 +0530

    Packages

---
+++ I/src/Lec36/Packages_/Packages/src/main/java/com/gla/Packages/PackagesApplication.java
@@ -0,0 +1,13 @@
+package com.gla.Packages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagesApplication.class, args);
+	}
+
+}
---
+++ I/src/Lec36/Packages_/Packages/src/test/java/com/gla/Packages/PackagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gla.Packages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
